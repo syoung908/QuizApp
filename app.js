e72d58a229904a54eb8db18517d7d5d410366523
Sean Young <syoung908@gmail.com>
2020-07-23 19:31:20 -0700

    Boilerplate Express
    Basic react hello word and DB setup.

---
+++ app.js
@@ -0,0 +1,37 @@
+const config = require('./config')
+const express = require('express');
+const path = require('path'); 
+const app = express();
+
+// Globals
+const PORT = process.env.PORT || 3000;
+const DIST_DIR = path.join(__dirname, '/public'); 
+const HTML_FILE = path.join(DIST_DIR, 'index.html');
+
+// Establish Database Connection
+const mongoose = require('mongoose');
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connect(config.DBConnectionString, (err) => {
+  if (err) console.error(err);
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(DIST_DIR));
+
+var apiRouter = require('./routes/api');
+app.use('/api', apiRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(HTML_FILE)
+});
+
+app.listen(PORT, function () {
+  console.log('Server listening on port: ' + PORT);
+  console.log(`mode: ${process.env.NODE_ENV}`);
+});
